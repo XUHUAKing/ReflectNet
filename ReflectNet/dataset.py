dffc8c9bb5ad182ccd33f7e7ea48f81fcfcbe28d
King <xhuangat@connect.ust.hk>
2019-10-05 19:38:26 +0800

    support training by train.py

--- ReflectNet/dataset.py
+++ ReflectNet/dataset.py
@@ -2,30 +2,67 @@
 Author: xuhua.huang
 """
 from tensorpack import *
+from glob import glob
 import argparse
 import cv2
 import numpy as np
+import tensorflow as tf
+import scipy
+import os
 
-class RealDataFlow(DataFlow):
-    # currently this is a hardcode dataset
-    def __init__(self, i0, i45, i90):
-        self.i0 = i0
-        self.i45 = i45
-        self.i90 = i90
-        self.lt = i0
-        self.lr = i0
+class MyDataFlow(DataFlow):
+    def __init__(self, data_path=None):
+        if not os.path.isdir(data_path):
+            raise Exception('data path not exist')
+        self.items = []
+        img_names = glob(data_path+'/*.npy')
+        img_names.sort()
+        mask_names = glob(data_path+'/mask' + '/*mask.png')
+        mask_names.sort()
+        for tmp_mask in mask_names:
+            tmp_M = data_path + '/IMG_' + tmp_mask[-18:-14] + '.npy'
+            tmp_R = data_path + '/IMG_' + tmp_mask[-13:-9] + '.npy'
+            if os.path.isfile(tmp_M) and os.path.isfile(tmp_R):
+                self.items.append([tmp_M,tmp_R,tmp_mask])
+            else:
+                print(tmp_M, tmp_R, tmp_mask, 'not exist...')
+                raise Exception('M/R/Mask not exist')
+        print("Data load succeed!")
 
     def __iter__(self):
-        i0 = self.i0
-        i45 = self.i45
-        i90 = self.i90
-        lt = self.lt
-        lr = self.lr
-        # load data from python and return
-        yield [i0, i45, i90, lt, lr]
+        for item in self.items:
+            M_name, R_name, mask_name = item
+            tmp_M = np.load(M_name) # (1, 1024, 1224, 5) = 1*H*W*5
+            tmp_R = np.load(R_name) # (1, 1024, 1224, 5)
+            tmp_mask=scipy.misc.imread(mask_name,'L')[::2,::2,np.newaxis]/255.
+            # get polarized image
+            i0 = tmp_M[0,:,:,0]
+            i45 = tmp_M[0,:,:,1]
+            i90 = tmp_M[0,:,:,2]
+            # crop glass area
+            i0 = i0[:,:,np.newaxis]*tmp_mask
+            i45 = i45[:,:,np.newaxis]*tmp_mask
+            i90 = i90[:,:,np.newaxis]*tmp_mask
+            # duplicate along RGB channel
+            I0 = np.tile(i0,[1,1,3])
+            I45 = np.tile(i45,[1,1,3])
+            I90 = np.tile(i90,[1,1,3])
+            # get lt and lr
+            M = 0.5*tmp_M[0,:,:,-1]
+            R = 0.5*tmp_R[0,:,:,-1]
+            T = M - R
+            # crop glass area
+            T = T[:,:,np.newaxis]*tmp_mask
+            T[T<0] = 0
+            R = R[:,:,np.newaxis]*tmp_mask
+            # duplicate along RGB channel
+            lt = np.tile(T,[1,1,3])
+            lr = np.tile(R,[1,1,3])
+            yield[I0, I45, I90, lt, lr]
 
     def __len__(self):
-        return 10
+        print(len(self.items))
+        return len(self.items)
 
     def get_data(self):
         return self.__iter__()
--- ReflectNet/train.py
+++ ReflectNet/train.py
@@ -2,25 +2,14 @@
 # -*- coding: utf-8 -*-
 
 """
-Copyright (C) 2018 NVIDIA Corporation.  All rights reserved.
-Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode).
-Authors: Patrick Wieschollek, Orazio Gallo, Jinwei Gu, and Jan Kautz
+Author: xuhua.huang
 """
 
 """
-evaluate model on given image
+train model on own dataset
+total batch size = (batch size of input source) * #GPU
 
-prepare data:
----------------------
-cd example
-dcraw -v -W -g 1 1 -6 *.ARW
-cd ..
-i0=example/DSC01908.ppm
-i45=example/DSC01909.ppm
-i90=example/DSC01910.ppm
-prefix=bar
-scale=0.25
-python eval.py --scale ${scale} --i0 ${i0} --i45 ${i45} --i90 ${i90} --out example/ --prefix ${prefix}
+python train.py --data ../../data_new/data_all_0609/20190609/Indoor/1/ --batch 32 --gpus 0
 
 """
 
@@ -32,102 +21,53 @@ from tensorpack import *
 import time
 import glob
 from contextlib import contextmanager
-from dataset import RealDataFlow
+from dataset import MyDataFlow
 import model
 import tensorflow as tf
 
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"]="1"  # specify which GPU(s) to be used
-
-@contextmanager
-def benchmark(name="unnamed context"):
-    """timing
-    Args:
-        name (str): output-name for timing
-    Example:
-        with meta.benchmark('doing heavy stuff right now tooks'):
-            sleep(1)
-    """
-    elapsed = time.time()
-    yield
-    elapsed = time.time() - elapsed
-    print('[{}] finished in {} ms'.format(name, int(elapsed * 1000)))
-
+os.environ["CUDA_VISIBLE_DEVICES"]="0"  # specify which GPU(s) to be used
 
 def main(args):
     logger.auto_set_dir()
-    assert os.path.isdir(args.out)
-
-    if not os.path.isfile(args.i0):
-        ppms = glob.glob(args.out + "/*.ppm")
-        ppms = sorted(ppms)
-        I0 = cv2.imread(ppms[0]).astype(np.float32) / 255.
-        I45 = cv2.imread(ppms[1]).astype(np.float32) / 255.
-        I90 = cv2.imread(ppms[2]).astype(np.float32) / 255.
-    else:
-        print("****path file exist, the path is correct")
-        assert os.path.isfile(args.i0)
-        assert os.path.isfile(args.i45)
-        assert os.path.isfile(args.i90)
-        I0 = cv2.imread(args.i0).astype(np.float32) / 255.
-        I45 = cv2.imread(args.i45).astype(np.float32) / 255.
-        I90 = cv2.imread(args.i90).astype(np.float32) / 255.
-
-    if args.scale is not 1.0:
-        I0 = cv2.resize(I0, (0, 0), fx=args.scale, fy=args.scale)
-        I45 = cv2.resize(I45, (0, 0), fx=args.scale, fy=args.scale)
-        I90 = cv2.resize(I90, (0, 0), fx=args.scale, fy=args.scale)
-
-    h, w, _ = I0.shape
-    h = (h // 8) * 8
-    w = (w // 8) * 8
-
-    # I0 = tf.tile(I0,[1,1,3])
-    # I45 = tf.tile(I45,[1,1,3])
-    # I90 = tf.tile(I90,[1,1,3])
-
-    I0 = I0[:h, :w, :]
-    I45 = I45[:h, :w, :]
-    I90 = I90[:h, :w, :]
 
     global HSHAPE, WSHAPE
-    model.HSHAPE, model.WSHAPE = I0.shape[:2]
-
-    I0 = I0[None, :, :, :]
-    I45 = I45[None, :, :, :]
-    I90 = I90[None, :, :, :]
-    # (1, 1000, 1504, 9)
+    model.HSHAPE, model.WSHAPE = 1024, 1224
 
     # build dataset
-    dataset_train = RealDataFlow(I0, I45, I90)
+    dataset_train = MyDataFlow(args.data)
+    # group data into batches of size 32
+    dataset_train = BatchData(dataset_train, args.batch)
+    # set up gpus
+    os.environ["CUDA_VISIBLE_DEVICES"]=args.gpus  # specify which GPU(s) to be used
+    gpus = [int(x) for x in args.gpus.split(',')]
+    batch_size = args.batch * len(gpus)
+    print('total batch size is: ', batch_size)
 
-    steps_per_epoch = len(dataset_train)
+    steps_per_epoch = -(-len(dataset_train)//batch_size) # get ceiling of division
     config = TrainConfig(
         model=model.Model(model.HSHAPE, model.WSHAPE),
         session_init=get_model_loader('data/checkpoint'),
         # The input source for training. FeedInput is slow, this is just for demo purpose.
         # In practice it's best to use QueueInput or others. See tutorials for details.
-        data=FeedInput(dataset_train),
+        data=QueueInput(dataset_train),
         callbacks=[
             ModelSaver(),   # save the model after every epoch
         ],
         steps_per_epoch=steps_per_epoch,
         max_epoch=12,
     )
-    launch_train_with_config(config, SimpleTrainer())
+    trainer = SyncMultiGPUTrainerReplicated(gpus)
+    launch_train_with_config(config, trainer)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--scale', help='scaling of inputs',
-                        type=float, default=1.0)
-    parser.add_argument('--out', help='output dir', type=str, default='/tmp')
-    parser.add_argument('--prefix', help='prefix', type=str, required=True)
-    parser.add_argument(
-        '--i0', help='image at 0 polarization', type=str, default='')
-    parser.add_argument(
-        '--i45', help='image at 45 polarization', type=str, default='')
-    parser.add_argument(
-        '--i90', help='image at 90 polarization', type=str, default='')
+    parser.add_argument('--data', help='data path',
+                        type=str, default='',required=True)
+    parser.add_argument('--gpus', help='gpu ids',
+                        type=str, default='0',required=True)
+    parser.add_argument('--batch', help='batch size',
+                        type=int, default=32)
     args = parser.parse_args()
     main(args)
